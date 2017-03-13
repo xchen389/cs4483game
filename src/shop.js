a200aa386098f9227146f051c8f6cca1d089d07f
atherqureshi <atherqureshi07@gmail.com>
2017-03-13 19:14:35 -0400

    added exit button to shop screen that takes user back to mainmenu

--- src/shop.js
+++ src/shop.js
@@ -3,14 +3,21 @@ var shop = {
 	preload: function(){
 		this.load.image('shopScreen','./assets/images/shop_screen.png');
 		this.load.image('nextButton','./assets/images/next_button.png');
+		this.load.image('exitButton','./assets/images/exit_button.png');
 	},
 
 	create: function(){
 		this.add.tileSprite(0,0, 1280, 800, 'shopScreen');
-		this.add.button(750,650, 'nextButton', this.loadGame,this);
+		//make buttons smaller later 
+		this.add.button(580,650, 'nextButton', this.loadGame,this);
+		this.add.button(920,650, 'exitButton', this.loadMenu,this);
 	},
 
 	loadGame: function(){
 		this.state.start('game');
+	},
+
+	loadMenu: function(){
+		this.state.start('menu');
 	}
 }
