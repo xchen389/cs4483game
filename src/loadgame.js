0b1633c1fe5434c0c8dfc9be5b6ce754a5bf7672
Ather Qureshi <atherqureshi07@gmail.com>
2017-03-19 18:07:14 -0400

    able to show load file on screen (only one right now)

--- src/loadgame.js
+++ src/loadgame.js
@@ -8,6 +8,7 @@ var loadGame = {
 		this.load.image('loadButton', './assets/images/buttons/load_button.png');
 		this.load.image('deleteButton', './assets/images/buttons/delete_button.png');
 		this.load.image('deleteAllButton', './assets/images/buttons/deleteAll_button.png');
+		this.load.image('blankButton', './assets/images/buttons/blank_button.png');
 		this.load.audio('introMusic', './assets/sounds/introMusic.ogg');
 	},
 
@@ -15,8 +16,8 @@ var loadGame = {
 		this.add.tileSprite(0,0, 1280, 800, 'loadGameBackground');
 		this.add.button(50,690, 'backButton', this.goBack,this);
 		this.add.button(960,690, 'loadButton', this.loadTheGame,this);
-		this.add.button(228, 690, 'deleteAllButton', this.deleteAllfunction());
-		this.add.button(455, 690, 'deleteButton', this.deleteFunction());
+		this.add.button(355, 690, 'deleteAllButton', this.deleteAllfunction, this);
+		this.add.button(655, 690, 'deleteButton', this.deleteFunction, this);
 
 		
 		//music settings
@@ -26,15 +27,22 @@ var loadGame = {
 			introMusic.play();
 		}
 
+		
 		//Get array of names in local Storage
 		nameKeys = returnAllData();
 
-		//create sprites with data file as name, and can be clicked on
-		for(int i = 0; i < nameKeys.length, i++){
-			game.add.sprite(400, i*50)
+		//create sprites that can be clicked on, with text according to name centered on
+		for(var i = 0; i < nameKeys.length; i++){
+			var tempDataButton = this.add.sprite(490, 330+i*50, 'blankButton');
+			tempDataButton.inputEnabled = true;
+			tempDataButton.height = 50;
+			var tempText = this.add.text(0,0, nameKeys[i]);
+			tempText.anchor.set(0.5);
+			tempText.x = Math.floor(tempDataButton.x + tempDataButton.width/2);
+			tempText.y = Math.floor(tempDataButton.y + tempDataButton.height/2);
 		}
 
-
+		
 		//list the names in the screen and let the user select a name
 		//and then load that saveFile corresponding to that name
 	},
@@ -48,12 +56,12 @@ var loadGame = {
 	},
 
 	shutdown: function(){
-	}
+	},
 
 	//TO-DO Show user feedback and verification
 	deleteAllfunction: function(){
 		deleteAll();
-	}
+	},
 
 	//take the name the user currently has selected, 
 	// and use that as key to delete function
