3b9c0bee9b22b9443fecca0ba12d10d184a7914a
Ather Qureshi <atherqureshi07@gmail.com>
2017-03-26 17:06:33 -0400

    just added to the hover and pressdown sounds to the gameover, I forgot

--- src/gameover.js
+++ src/gameover.js
@@ -6,7 +6,13 @@ aotb_game.gameover = function(){
 
 	this.create = function(){
 		this.add.tileSprite(0,0, 1280, 800, 'gameoverBackground');
-		this.add.button(170,510, 'mainmenuButton', loadMainMenu,this);
+		var mainMenuButton = this.add.button(170,510, 'mainmenuButton', loadMainMenu,this);
+
+		var buttonPressSound = this.add.audio('buttonClickSound');
+		var buttonHoverSound = this.add.audio('buttonHoverSound');
+
+		mainMenuButton.setDownSound(buttonPressSound);
+		mainMenuButton.setOverSound(buttonHoverSound);
 	};
 
 	function loadMainMenu(){
